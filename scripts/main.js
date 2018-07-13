b1dea293c9164e5300837807ccab3bfaea5c91cd
1337menohax <vivu493@gmail.com>
2018-07-12 19:50:25 -0700

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,102 @@
+var DETAIL_IMAGE_SELECTOR = "[data-image-role=\"target\"]";
+var DETAIL_TITLE_SELECTOR = "[data-image-role=\"title\"]";
+var DETAIL_FRAME_SELECTOR = "[data-image-role=\"frame\"]";
+var THUMBNAIL_LINK_SELECTOR = "[data-image-role=\"trigger\"]";
+var HIDDEN_DETAIL_CLASS = "hidden-detail";
+var TINY_EFFECT_CLASS = "is-tiny";
+var ESC_KEY = 27;
+var picIndex = 1;
+
+function movePic(n) {
+  showPic(picIndex += n);
+}
+function showPic(n) {
+  var picArray = getThumbnailsArray();
+  if (n > picArray.length) {
+    picIndex = 1;
+  }
+  if (n < 1) {
+    picIndex = picArray.length;
+  }
+
+  setDetailsFromThumb(picArray[picIndex - 1]);
+  showDetails();
+}
+
+function setDetails(imageUrl, titleText) {
+  "use strict";
+
+  var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+  detailImage.setAttribute("src", imageUrl);
+
+  var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+  detailTitle.textContent = titleText;
+}
+
+function imageFromThumb(thumbnail) {
+  "use strict";
+  return thumbnail.getAttribute("data-image-url");
+}
+
+function titleFromThumb(thumbnail) {
+  "use strict";
+  return thumbnail.getAttribute("data-image-title");
+}
+
+function setDetailsFromThumb(thumbnail) {
+  "use strict";
+  setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
+}//sending thumbnail to detail after clicking
+
+
+
+function addThumbClickHandler(thumb) {
+  thumb.addEventListener("click", function (event) {
+    event.preventDefault();
+    setDetailsFromThumb(thumb);
+    showDetails();
+
+  });
+}//what will happen if you click
+
+function getThumbnailsArray() {
+  "use strict";
+  var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+  var thumbnailArray = [].slice.call(thumbnails);
+  return thumbnailArray;
+}
+function hideDetails() {
+  "use strict";
+  document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+
+function showDetails() {
+  "use strict";
+  var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
+  document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+  frame.classList.add(TINY_EFFECT_CLASS);
+  setTimeout(function () {
+    frame.classList.remove(TINY_EFFECT_CLASS);
+  }, 50);
+}//add effect to detail image
+
+function addKeyPressHandler() {
+  "use strict";
+  document.body.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === ESC_KEY) {
+      hideDetails();
+    }
+  });
+}//change screen when ESC is pressed
+
+function initializeEvents() {
+  "use strict";
+  var thumbnails = getThumbnailsArray();
+  thumbnails.forEach(addThumbClickHandler);
+  addKeyPressHandler();
+}
+
+
+
+initializeEvents();
